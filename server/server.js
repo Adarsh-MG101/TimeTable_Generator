e776e7aae0d0303668ef15d427a72c1fecda4f3c
Adarsh-MG101 <officialamg100@gmail.com>
2025-08-26 18:40:48 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import teacherRoutes from "./routes/teacherRoutes.js";
+import subjectRoutes from "./routes/subjectRoutes.js";
+import timetableRoutes from "./routes/timetableRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("MongoDB connection error:", error));
+
+app.use("/api/teachers", teacherRoutes);
+app.use("/api/subjects", subjectRoutes);
+app.use("/api/timetables", timetableRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
